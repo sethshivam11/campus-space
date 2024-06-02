e520d2f392e6b1773448ae3c5ec5fc1704c56cb3
mukilan2815 <mukilan2808@gmail.com>
2024-06-02 08:45:52 +0530

    absent teacher table done

--- client/src/components/TeachersAbsent.tsx
+++ client/src/components/TeachersAbsent.tsx
@@ -1,59 +1,44 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import { Card, CardTitle, CardHeader, CardContent } from "./ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "./ui/table";
 import { useUser } from "@/context/UserProvider";
-
+import { useMantineReactTable, MRT_ColumnDef } from "mantine-react-table";
+import { MantineReactTable } from "mantine-react-table";
 function TeachersAbsent() {
   const { teachersAbsent, getTeachersAbsent } = useUser();
 
-  React.useEffect(() => {
-    getTeachersAbsent()
-  }, [])
+  useEffect(() => {
+    getTeachersAbsent();
+  }, [getTeachersAbsent]);
+
+  const columns = useMemo<MRT_ColumnDef<any>[]>(
+    () => [
+      {
+        accessorKey: "fullName",
+        header: "Teacher",
+      },
+      {
+        accessorKey: "email",
+        header: "Email",
+      },
+    ],
+    []
+  );
+
+  const table = useMantineReactTable({
+    columns,
+    data: teachersAbsent,
+  });
 
   return (
     <section className="min-h-screen min-w-screen">
-      <Card className="md:w-3/5 w-5/6 mx-auto dark:bg-zinc-900 bg-gray-200 my-6 select-none">
+      <Card className="md:w-3/5 w-5/6 mx-auto dark:bg-zinc-900 bg-gray-900 my-6 select-none">
         <CardHeader>
           <CardTitle className="text-2xl text-center">
             Teachers Absent
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Teacher</TableHead>
-                <TableHead>Email</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {teachersAbsent.length ? (
-                teachersAbsent.map((teacher, index) => {
-                  if (!teacher.isAdmin)
-                    return (
-                      <TableRow
-                        key={index}
-                        className="hover:bg-zinc-200 dark:hover:bg-zinc-800"
-                      >
-                        <TableCell>{teacher.fullName}</TableCell>
-                        <TableCell>{teacher.email}</TableCell>
-                      </TableRow>
-                    );
-                })
-              ) : (
-                <TableRow className="hover:bg-zinc-200 dark:hover:bg-zinc-800">
-                  <TableCell colSpan={2}>No teachers absent</TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
+          <MantineReactTable table={table} />
         </CardContent>
       </Card>
     </section>
